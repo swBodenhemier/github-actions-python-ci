201b7994c562ed2eb449ff2279f7bfc6743207df
SWBodenhemier <swb79@nau.edu>
2026-09-21 14:55:48 -0700

    Fix test error

--- tests/test_calculator.py
+++ tests/test_calculator.py
@@ -15,7 +15,7 @@ class TestCalculator(unittest.TestCase):
 
     def test_divide(self):
         self.assertEqual(divide(6, 2), 3)
-        self.assertEqual(divide(2, -1), -1)
+        self.assertEqual(divide(2, -1), -2)
         self.assertEqual(divide(4, 0), 0)
 
 if __name__ == '__main__':
