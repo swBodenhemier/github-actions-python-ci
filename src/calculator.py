b79a39ec97519b4e5efd54f1792ed79f3b87aa94
SWBodenhemier <swb79@nau.edu>
2026-09-21 14:47:08 -0700

    Initial commit

---
+++ src/calculator.py
@@ -0,0 +1,10 @@
+def add(a, b):
+    return a + b
+
+def subtract(a, b):
+    return a - b
+
+def divide(a, b):
+    if b == 0:
+        return 0
+    return a / b
---
+++ tests/test_calculator.py
@@ -0,0 +1,22 @@
+import unittest
+from src.calculator import add, subtract, divide 
+
+class TestCalculator(unittest.TestCase):
+    def test_add(self):
+        self.assertEqual(add(2, 3), 5)
+        self.assertEqual(add(-1, 1), 0)
+        self.assertEqual(add(-1, -1), -2)
+
+    def test_subtract(self):
+        self.assertEqual(subtract(5, 2), 3)
+        self.assertEqual(subtract(-2, -4), 2)
+        self.assertEqual(subtract(3, -3), 6)
+        self.assertEqual(subtract(-2, 4), -6)
+
+    def test_divide(self):
+        self.assertEqual(divide(6, 2), 3)
+        self.assertEqual(divide(2, -1), -1)
+        self.assertEqual(divide(4, 0), 0)
+
+if __name__ == '__main__':
+   unittest.main() 
